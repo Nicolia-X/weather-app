2f5b2bd21e42247dea44c332c523277e4eb7d6b9
Nicolia-X <168901828+Nicolia-X@users.noreply.github.com>
2024-06-22 22:50:11 +0200

    Injected weather forecast data on the Html, Css and Javascript using API integration and JS template

--- src/index.js
+++ src/index.js
@@ -16,26 +16,9 @@ function updateTemperature(response) {
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="current-temperature-icon" />`;
   timeElement.innerHTML = `${formatDate(date)} `;
+
+  getForecast(response.data.city);
 }
-
-function searchCity(city) {
-  let apiKey = "38fd65bf84o27e30f1bfb1a6d59t9604";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-
-  axios.get(apiUrl).then(updateTemperature);
-}
-
-function searchCitySubmit(event) {
-  event.preventDefault();
-  let searchInputElement = document.querySelector("#search-input");
-
-  searchCity(searchInputElement.value);
-}
-
-let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", searchCitySubmit);
-
-searchCity("Johannesburg");
 
 function formatDate(date) {
   let minutes = date.getMinutes();
@@ -60,3 +43,65 @@ function formatDate(date) {
 
   return `${day} ${hours}:${minutes}`;
 }
+
+function searchCity(city) {
+  let apiKey = "38fd65bf84o27e30f1bfb1a6d59t9604";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(updateTemperature);
+}
+
+function searchCitySubmit(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-input");
+
+  searchCity(searchInputElement.value);
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
+function getForecast(city) {
+  let apiKey = "38fd65bf84o27e30f1bfb1a6d59t9604";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
+  let forecastHtml = "";
+
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
+      <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${formatDay(day.time)}</div>
+
+        <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature">
+            <strong>${Math.round(day.temperature.maximum)}º |</strong>
+          </span>
+          <span class="weather-forecast-temperature">${Math.round(
+            day.temperature.minimum
+          )}ºC</span>
+        </div>
+      </div>
+    `;
+    }
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", searchCitySubmit);
+
+searchCity("Johannesburg");
